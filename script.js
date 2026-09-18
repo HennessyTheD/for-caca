10c25c8986cb859f564a096fae2b20ba346f9921
HennessyTheD <rkurniawan1905@gmail.com>
2026-09-18 20:32:45 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `Hiii Cacaa.. i just want u to know that..\nU really did well for todayy, not everyone can handle things like u do\n\nlet me know if u need anything to cheer u up, i'll do my bestt. and there's something that i wanna say\n\nI wanna say Thank You Sooo MUCHHH for accepted my friend request caa. You're very cutee, i feel lucky to know you\n\nAnyway, i cant wait to hangout to see u!`;
+const message = `Hiii Cacaa.. i just want u to know that..\nU really did well for todayy, not everyone can handle things like u do\n\nlet me know if u need anything to cheer u up, i'll do my bestt. And there's something that i wanna say\n\nI wanna say Thank You Sooo MUCHHH for accepted my friend request caa. You're very cutee, i feel lucky to know you\n\nAnyway, i cant wait to see u!`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
